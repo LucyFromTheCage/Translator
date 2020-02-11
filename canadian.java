8ce454fc3bed69ee2abfb07675d09c5da0e7302f
LucyFromTheCage <60931839+LucyFromTheCage@users.noreply.github.com>
2020-02-11 09:18:45 -0600

    Delete canadian.java

--- canadian.java
+++
@@ -1,8 +0,0 @@
-public class canadian{
-    private String ending = ", eh";
-    private String words;
-
-    public canadian(String sentence) { words = sentence; }
-
-    public String toString() { return words + " " + ending; }
-}
